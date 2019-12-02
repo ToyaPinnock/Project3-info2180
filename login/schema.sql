d9361f60e8e0ec63863497c790773190560c2c2c
toyapinnock <toyapinnock@gmail.com>
2019-12-01 20:32:09 -0500

    final update

--- login/schema.sql
+++ login/schema.sql
@@ -1,18 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2019 at 01:42 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.2.24
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `tracker` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `tracker`;
+--
+-- Database: `tracker`
+--
 
-CREATE TABLE IF NOT EXISTS `issues` (
-  `id` int(1) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issues`
+--
+
+CREATE TABLE `issues` (
+  `id` int(1) NOT NULL,
   `title` varchar(200) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `type` varchar(100) DEFAULT NULL,
@@ -20,23 +37,69 @@ CREATE TABLE IF NOT EXISTS `issues` (
   `status` varchar(100) DEFAULT NULL,
   `assigned_to` varchar(100) DEFAULT NULL,
   `created_by` varchar(100) DEFAULT NULL,
-  `created` varchar(100) DEFAULT NULL,
-  `updated` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(1) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(1) NOT NULL,
   `firstname` varchar(100) DEFAULT NULL,
   `lastname` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `date_joined` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `date_joined` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `email`, `date_joined`) VALUES
-(1, '', '', 'password123', 'admin@bugme.com', NULL);
+(1, '', '', 'password123', 'admin@bugme.com', NULL),
+(2, 'John', 'Brown', 'Ferws123', 'johnbrown@bugme.com', NULL),
+(3, 'Johnny', 'case', 'password3eds', 'johnnycase@bugme.com', NULL),
+(4, 'John', 'case', 'kemmid', 'jc@bugme.com', NULL),
+(5, 'John', 'Brown', 'kevin21', 'johnbrown@bugme.com', NULL),
+(6, 'Toya', 'pinnock', 'toya123', 'toyapinnock@gmail.com', NULL),
+(7, 'gavin', 'escoe', 'qwer', 'dalton@bugme.com', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `issues`
+--
+ALTER TABLE `issues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `issues`
+--
+ALTER TABLE `issues`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
